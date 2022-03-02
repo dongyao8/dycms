cf6b574fc1d6c98c3e63608fbb47938262a5c3f0
clark <243512842@qq.com>
2022-03-02 19:38:38 +0800

    修复登录页

--- template/admin/login/index.blade.php
+++ template/admin/login/index.blade.php
@@ -1,41 +1,52 @@
 <!DOCTYPE html>
-<html lang="zxx">
-
+<html lang="en">
 <head>
-    <title>登录账号</title>
-    <!-- Meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- //Meta tags -->
-    <link rel="stylesheet" href="{{config('static.path')}}login/css/style.css" type="text/css" media="all" /><!-- Style-CSS -->
-    <link href="{{config('static.path')}}/login/css/font-awesome.css" rel="stylesheet"><!-- font-awesome-icons -->
+    <title>{{env('APP_NAME')}} - 后台管理</title>
+    <meta name="author" content="David Grzyb">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="{{$dycms_static}}amis/{{env('ADMIN_THEME', 'sdk')}}.css" />
+    <!-- Tailwind -->
+    <link href="https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/tailwindcss/2.0.3/tailwind.min.css" rel="stylesheet">
+    <style>
 
-    <link rel="stylesheet" href="{{config('static.path')}}/amis/{{env('ADMIN_THEME', 'sdk')}}.css" />
-    <link rel="stylesheet" href="{{config('static.path')}}/amis/helper.css" />
-    <link rel="stylesheet" href="{{config('static.path')}}/amis/iconfont.css" />
+        .font-family-karla {
+            font-family: karla;
+        }
+    </style>
 </head>
-<style>
-    .form-36-mian {
-        background-image: linear-gradient(to right, rgba(0, 0, 0, 0.85), rgba(255, 255, 255, 0)), url({{$days['images']}});
-    }
-    .wrapper{
-        max-width: 540px;
-    }
-</style>
+<body class="bg-white font-family-karla h-screen">
 
-<body>
-<section class="w3l-form-36">
-    <div class="form-36-mian section-gap">
-        <div class="wrapper">
-            <div class="form-inner-cont">
-                <h3>登录账号</h3>
-                <div id="root" class="app-wrapper"></div>
+<div class="w-full flex flex-wrap">
+
+    <!-- Login Section -->
+    <div class="w-full md:w-1/2 flex flex-col">
+
+        <div class="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
+            <a class="bg-black text-white font-bold text-xl p-4">{{env('APP_NAME')}}</a>
+        </div>
+
+        <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
+            <p class="text-center text-3xl">登录后台</p>
+            <div id="loginform" class="app-wrapper"></div>
+
+            <div class="text-center pt-12 pb-12">
+                <p class="text-gray-500">Copyright © 2019-{{date('Y')}}<a target="_blank" href="https://www.dongyao.ren/" class="underline font-semibold"> Clark</a>. All rights reserved.</p>
             </div>
         </div>
+
     </div>
-</section>
-<script src="{{config('static.path')}}/amis/sdk.js"></script>
+
+    <!-- Image Section -->
+    <div class="w-1/2 shadow-2xl">
+        <img class="object-cover w-full h-screen hidden md:block" src="{{$days['images']}}">
+    </div>
+</div>
+
+
+
+<script src="{{$dycms_static}}/amis/sdk.js"></script>
 <script type="text/javascript">
     (function() {
         let amis = amisRequire('amis/embed');
@@ -76,19 +87,18 @@
                 },
                 "messages": {
                     "fetchFailed": "初始化失败",
-                    "saveSuccess": "保存成功",
-                    "saveFailed": "保存失败",
+                    "saveSuccess": "登录成功",
+                    "saveFailed": "登录失败",
                     "fetchSuccess": "登录成功",
                     "validateFailed": "登录失败"
                 }
             }
         };
-        let amisScoped = amis.embed('#root', amisJSON);
+        let amisScoped = amis.embed('#loginform', amisJSON);
         amisScoped.updateProps({
             theme: "{{env('ADMIN_THEME', 'sdk')}}"
         });
     })();
 </script>
 </body>
-
 </html>

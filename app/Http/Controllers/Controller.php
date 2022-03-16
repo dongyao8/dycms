7d07e5458439b6c74cbac28c4f15ca456811185d
dongyao <dongyao@dongyao.local>
2022-03-16 23:01:48 +0800

    API接口格式化

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //API接口返回格式化
+    public function apiReturn($code=0,$msg='success',$data=[]){
+        return response()->json([
+            'code' => $code,
+            'msg' => $msg,
+            'data' => $data
+        ]);
+    }
 }

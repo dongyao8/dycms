778f0f06bc89aebd9d633533415fdab7471b91aa
clark <243512842@qq.com>
2022-03-18 17:27:31 +0800

    修改密码

---
+++ app/Http/Controllers/Admin/MimaController.php
@@ -0,0 +1,87 @@
+<?php
+
+    namespace App\Http\Controllers\Admin;
+
+    use App\Http\Controllers\Controller;
+    use Illuminate\Http\Request;
+    use App\Models\Admin;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Support\Facades\Hash;
+
+    class MimaController extends Controller
+    {
+        //修改后台密码
+        public function body()
+        {
+            $body[] = [
+                'type' => "form",
+                'title' => '修改密码',
+                'api' => route('admin.handdle.safe.upmm'),
+                'body' => array(
+                    [
+                        'type' => 'input-password',
+                        'name' => 'o_pwd',
+                        'label' => '原密码',
+                        "required" => true,
+
+                    ],
+                    [
+                        'type' => 'input-password',
+                        'name' => 'n_pwd',
+                        'label' => '新密码',
+                        "required" => true,
+
+                    ],
+                    [
+                        'type' => 'input-password',
+                        'name' => 'n_pwd2',
+                        'label' => '重复新密码',
+                        "required" => true,
+
+                    ]
+                )
+            ];
+
+            $data = [
+                'type' => 'page',
+                'title' => '修改密码',
+                'body' => $body,
+            ];
+            return $data;
+        }
+
+        //    修改密码逻辑
+        public function upmm(Request $request)
+        {
+            $o_pwd = $request->input('o_pwd');
+            $n_pwd = $request->input('n_pwd');
+            $n_pwd2 = $request->input('n_pwd2');
+
+            if (strlen($o_pwd) < 6 || strlen($n_pwd) < 6) {
+                return $this->apiReturn(100,'密码不能少于6位','');
+            }
+
+            if (!ctype_alnum($n_pwd)) {
+                return $this->apiReturn(100,'密码只能是字母和数字组合','');
+            }
+
+            if ($n_pwd != $n_pwd2) {
+                return $this->apiReturn(100,'两次密码不一致','');
+            }
+            //        验证旧密码
+            $admin = Auth::guard('admin')->user();
+            if (!Hash::check($o_pwd, $admin->password)) {
+                return $this->apiReturn(100,'原密码错误','');
+            }
+            //        修改密码
+            $upmm = Admin::find($admin->id);
+            $upmm->password = Hash::make($n_pwd);
+            $res = $upmm->save();
+            if ($res) {
+                return $this->apiReturn(0,'修改成功','');
+            } else {
+                return $this->apiReturn(100,'系统出错','');
+            }
+        }
+
+    }
--- routes/admin.php
+++ routes/admin.php
@@ -38,19 +38,14 @@ Route::get('gout',function (){
 Route::prefix('handdle')->name('handdle.')->group(function () {
     // 修改密码
     Route::prefix('safe')->name('safe.')->group(function () {
-        Route::post('/', [Admin\SystemController::class, 'upmm'])->name('upmm');
+            Route::post('/upmm', [Admin\MimaController::class, 'upmm'])->name('upmm');
     });
 
-// 用户管理
+    // 用户管理
     Route::prefix('user')->name('user.')->group(function () {
         Route::post('/educheck', [Admin\UserController::class, 'eduCheck'])->name('educheck'); //增加查询额度
         Route::get('/datalist', [Admin\UserController::class, 'datalist'])->name('datalist'); //用户数据
         Route::post('/single', [Admin\UserController::class, 'single'])->name('single'); //更新用户信息
-    });
-
-    // 报告
-    Route::prefix('report')->name('report.')->group(function () {
-        Route::get('/datalist', [Admin\ReportController::class, 'datalist'])->name('datalist'); //报告数据
     });
 
 });

3a62e807c9f51a56f4837ce6db24c55f4eca4842
clark <243512842@qq.com>
2022-06-30 13:13:35 +0800

    增加日志记录方法

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
 
 class Controller extends BaseController
 {
@@ -20,11 +21,26 @@ class Controller extends BaseController
      * @param $state string 状态字段说明，部分接口需求状态码字段为status,有的又是code
      * @return \Illuminate\Http\JsonResponse
      */
-    public function apiReturn($status=0,$msg='success',$data=[],$state='status'){
+    public function apiReturn($status = 0, $msg = 'success', $data = [], $state = 'status')
+    {
         return response()->json([
             $state => $status,
             'msg' => $msg,
             'data' => $data
         ]);
+    }
+
+    // 日志记录
+    /**
+     * dir 日志目录
+     * type 日志类型：emergency、 alert、 critical、 error、 warning、 notice、 info 和 debug
+     * content 日志内容
+     */
+    public function addLogs($dir, $type, $content)
+    {
+        Log::build([
+            'driver' => 'daily',
+            'path' => storage_path('logs/' . $dir . '/' . $type . '.log'),
+        ])->$type($content);
     }
 }

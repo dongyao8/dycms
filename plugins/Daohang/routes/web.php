290588ddcb765b1bf498978551ea47b4ceb56ba6
clark <243512842@qq.com>
2022-07-05 20:28:35 +0800

    添加演示路由

---
+++ plugins/Daohang/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('daohang')->name('daohang.')->group(function () {
+
+    // 路由逻辑
+    Route::get('index',[\Plugins\Daohang\Controller\IndexController::class,'index']); //测试调试
+
+
+});
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,27 @@ Route::fallback(function (){
  */
 Route::get(env('ADMIN_PREFIX', 'admin').'/login', [\App\Http\Controllers\Admin\LoginController::class, 'index'])->name('admin.login'); //管理后台登录页
 Route::post(env('ADMIN_PREFIX', 'admin').'/login', [\App\Http\Controllers\Admin\LoginController::class, 'checklogin'])->name('admin.login.check')->middleware('adminlog'); //登录检测
+
+// 自动加载插件前端路由
+Route::prefix('plugins')->name('plugins.')->group(function () {
+    //START 动态加载插件路由
+    $pluginRoute = 'web.php';
+    $folder = 'routes';
+    $rootpath = base_path();
+    //拼接插件路由文件夹
+    $plgins = $rootpath . '/' . 'plugins/';
+    foreach (scandir($plgins) as $file) {
+        // 只处理文件夹内容
+        if (is_dir($plgins . $file)) {
+            //    屏蔽隐藏文件夹
+            if ($file != "." && $file != "..") {
+                //    判断文件存在，并加载
+                $fullPath = $plgins . $file . '/' . $folder . '/' . $pluginRoute;
+                if (file_exists($fullPath)) {
+                    require($fullPath); //引入指定文件
+                }
+            }
+        }
+    }
+    // END 动态加载路由
+});
